5cf7e3430a1cc03a5cd40e5c4c8b06f98cceda7d
ISSHHHH <sagarishu246@gmail.com>
2022-08-28 20:07:49 +0530

    Syntax Fix

--- src/pages/claim.jsx
+++ src/pages/claim.jsx
@@ -57,7 +57,7 @@ export default function Claim() {
                                   
                                     <iframe
                                         src={"https://gateway.ipfscdn.io/ipfs/Qma5FgmS9s3hkQrURfG4XDnh5Dxp9Fx9ZjBYkPFUNdQwfi/edition-drop.html?contract=0xD8E4775451E2Ba750cC7C5ec596a04E2d2eeF549&chainId=137&tokenId=0&relayUrl=https%3A%2F%2Fapi.defender.openzeppelin.com%2Fautotasks%2F9c183054-706f-4f67-baf7-b9100b7c8dad%2Fruns%2Fwebhook%2Ff1498096-a68b-451c-87a0-c06af19a6be9%2F9v3ihuCMCVm3hCaPSVu8NJ"}
-                                        frameBorder="0"
+                                        frameBorder={"0"}
                                         className={styles.nftCard}
 
                                     ></iframe>
