e58f755fd87875d5a53f925cf9583b4f4719141c
ISSHHHH <sagarishu246@gmail.com>
2022-08-28 21:14:54 +0530

    Iframe bug fixed

--- src/context/userStates.js
+++ src/context/userStates.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import UserContext from './userContext';
 
 const UserState = (props) => {
-    const [login, setLogin] = useState(true);
+    const [login, setLogin] = useState(false);
     return (
         <UserContext.Provider value={{ login, setLogin }}>
             {props.children}
